debc0887a6045c1c3ef07977ca26ba7b2881cafa
commenthol <commenthol@gmail.com>
2026-02-07 13:46:17 +0100

    chore(webcomponent): bump dependencies

--- packages/webcomponent/bin/storybook-tiny.js
+++ packages/webcomponent/bin/storybook-tiny.js
@@ -7,7 +7,7 @@ const config = {
   rootDir: fileURLToPath(new URL('..', import.meta.url)),
   devDependencies: [
     '@storybook-tiny/webcomponent',
-    'mi-element@0.7.x',
+    'mi-element@0.8.x',
     'vite',
     'split.js@^1'
   ],
